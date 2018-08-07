5e77c6fa8cc185be263f49fe4e19a058b1e43122
chanyue77 <jiebinc@163.com>
2018-08-07 23:34:36 +0800

    首页调整

--- js/main.js
+++ js/main.js
@@ -13,6 +13,20 @@ var mySwiper2= new Swiper('.about_pic', {
 	pagination: '.swiper-pagination2',
 	paginationClickable: true, 
 });
+
+$(window).resize(function(){
+	navaSetting();
+	var h = $(window).height();
+	if(h > 690){
+		$('.mnav1').css({'padding-bottom':'20px'});
+	}
+	else{
+		$('.mnav1').css({'padding-bottom':'0'});
+	}
+});
+$(window).resize();
+
+
 $(function(){
 	$("#hot_btn_more").click(function(){
 		$(".more_loading").show();
@@ -21,8 +35,91 @@ $(function(){
 			$(".more_loading").hide();
 			$("#hot_more").fadeIn(1000);
 		},300)
-	})
-})
+	});
+	navaSetting();
+	//checkNaviHide();
+	bottomNavtoggle();
+	
+});
+function navaSetting(){
+	var navw = 0;
+	for(var i=0;i<9;i++){
+		navw+=$(".nav2 a:eq("+i+")").width();
+	}
+	var ws = Math.floor(($(".nav2").width()-navw)/8);
+	for(var i=0;i<8;i++){
+		$(".nav2 a:eq("+i+")").css({"margin-right":ws+"px"});
+	}
+}
+function bottomNavtoggle(){
+	var otop = 950;
+	var timeout;
+	var state = 'show';
+	var ns = 'show';
+	var upflag = 1;
+	var downflag = 1;
+	$(window).scroll(function(){
+		console.log(otop,$(window).scrollTop())
+		clearTimeout(timeout);
+		var n = ($(window).scrollTop()+$(window).height()) - otop;
+        if(n < -5){
+        	 if (downflag) {
+				$('.bottom-Menu').animate({'bottom':'-100px'});
+				downflag = 0;
+				upflag = 1;
+            }
+    	}
+    	else if(n > -5){
+			if (upflag) {
+				$('.bottom-Menu').animate({'bottom':'0px'});
+				downflag = 1;
+				upflag = 0;
+			}
+    	}
+
+    	timeout = setTimeout(function(){
+    		currTop = $(window).scrollTop();
+    		state = ns;
+    	}, 80);
+
+    });
+}
+function checkNaviHide(){
+	var currTop = $(window).scrollTop();
+	var timeout;
+	var state = 'show';
+	var ns = 'show';
+	var upflag = 1;
+	var downflag = 1;
+	$(window).scroll(function(){
+		clearTimeout(timeout);
+		var n = $(window).scrollTop() - currTop;
+        if(n > 5){
+        	 if (downflag) {
+				$('.bottom-Menu').animate({'bottom':'-100px'});
+				downflag = 0;
+				upflag = 1;
+            }
+    	}
+    	else if(n < -5){
+			if (upflag) {
+				$('.bottom-Menu').animate({'bottom':'0'});
+				downflag = 1;
+				upflag = 0;
+			}
+    	}
+
+    	timeout = setTimeout(function(){
+    		currTop = $(window).scrollTop();
+    		state = ns;
+    	}, 80);
+
+    });
+
+
+}
+
+
 
 var hotcon = '';
 for(var i=0;i<4;i++){
